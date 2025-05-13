aedcf4329fc3451f16661fa76bb062ccedeaadea
ppsegur <pepesegurarodriguez@gmail.com>
2025-05-13 22:58:33 +0200

    feat: create CitasDashboard component to manage user information display

---
+++ Farma-Natur-app/src/components/Views/Farmaceutico/CitasDashboard.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+import { onMounted } from 'vue';
+import { jwtDecode } from 'jwt-decode';
+import FarmaHeader from '@/components/MicroComponents/Farmaceutico/FarmaHeader.vue';
+const userName = ref('');
+const userRole = ref('');
+
+onMounted(() => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    const decoded = jwtDecode(token);
+    userName.value = decoded.name || decoded.username || 'USERNAME';
+    userRole.value = decoded.role || 'FARMACEUTICO';
+  }
+});
+</script>
+
+<template>
+        <FarmaHeader :userName="userName" :userRole="userRole" />
+</template>
