65da09ee2c124a3f346a482431d43b7f9a9687bd
ppsegur <pepesegurarodriguez@gmail.com>
2025-05-14 21:22:59 +0200

    feat: add CitasDashboard component for managing appointments

--- Farma-Natur-app/src/components/Views/Farmaceutico/GestionesVistas/CitasDashboard.vue
+++ Farma-Natur-app/src/components/Views/Farmaceutico/GestionesVistas/CitasDashboard.vue
@@ -3,7 +3,7 @@ import { ref } from 'vue';
 import { onMounted } from 'vue';
 import { jwtDecode } from 'jwt-decode';
 import FarmaHeader from '@/components/MicroComponents/Farmaceutico/FarmaHeader.vue';
-import CitasTabla from './Citas/CitasTabla.vue';
+import CitasTabla from '../Citas/CitasTabla.vue';
 const userName = ref('');
 const userRole = ref('');
 
@@ -20,5 +20,12 @@ onMounted(() => {
 <template>
         <FarmaHeader :userName="userName" :userRole="userRole" />
 
+  <div class="dashboard-container" style="padding-top: 150px;">
+    <h1 class="dashboard-title">Gestión de Citas</h1>
+    <hr style="border: 1px solid #ccc; width: 100%;" />
+    <br>
+    <div style="display: flex; flex-direction: row; align-items: flex-start; padding-left: 100px; padding-right: 100px; padding-bottom: 100px;">
         <CitasTabla />
+    </div>
+    </div>
 </template>
